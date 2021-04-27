3a84e48d2c05f9dafcbf95160d51d20411867195
larissagiaccon <larissa_souz@hotmail.com>
2021-04-27 11:05:18 -0300

    Desafio Mobile GoRestaurant

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // baseURL: 'http://192.168.50.197:3333',
   baseURL: 'http://localhost:3333',
 });
 
 export default api;
+
+// yarn json-server --host 192.168.50.197 server.json -p 3333
